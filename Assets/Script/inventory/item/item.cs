fc59d1332a687f0fd267b787a9c31bbab98ea532
jzho987 <jzho987@aucklanduni.ac.nz>
2021-09-17 17:15:25 +1200

    feat: update on inventory development

---
+++ Assets/Script/inventory/item/item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new item", menuName = "item")]
+public class item : ScriptableObject
+{
+    //overview
+    [SerializeField] string itemName;
+    [SerializeField] string itemDescription;
+
+    //world item
+    [SerializeField] GameObject WorldItem;
+
+    //inventory item
+    [SerializeField] Sprite itemSprite;
+}
